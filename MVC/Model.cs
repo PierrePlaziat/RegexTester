3372e2a8bc97226015bc0a71049cde794f5a3028
PierrePlaziat <53494200+PierrePlaziat@users.noreply.github.com>
2019-08-27 16:28:19 +0200

    UpProject

---
+++ MVC/Model.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexTester.MVC
+{
+    class Model : INotifyPropertyChanged
+    {
+        #region NOTIFY PROPERTY
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+                Console.WriteLine(DateTime.Now + " Lm Widget Part >>> PropertyChanged : " + property);
+            }
+        }
+
+        #endregion
+
+
+        string regex;
+        public string Regex
+        {
+            get { return regex; }
+            set { regex = value; RaisePropertyChanged("Regex"); }
+        }
+
+        string fileContent;
+        public string FileContent
+        {
+            get { return fileContent; }
+            set { fileContent = value; RaisePropertyChanged("FileContent"); }
+        }
+
+        List<string> results = new List<string>();
+        public List<string> Results
+        {
+            get { return results; }
+            set { results = value; RaisePropertyChanged("Results"); }
+        }
+
+    }
+}
---
+++ MVC/View.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows;
+
+namespace RegexTester.MVC
+{
+    /// <summary>
+    /// Logique d'interaction pour View.xaml
+    /// </summary>
+    public partial class View : Window
+    {
+        Model model = new Model();
+
+        public View()
+        {
+            InitializeComponent();
+            DataContext = model;
+            Show();
+        }
+
+        private void ButtonExec_Click(object sender, RoutedEventArgs e)
+        {
+
+            RegexOptions options = 0;
+            if (Check_None.IsChecked == true) options = options | RegexOptions.None;
+            if (Check_Compiled.IsChecked == true) options = options | RegexOptions.Compiled;
+            if (Check_CultureInvariant.IsChecked == true) options = options | RegexOptions.CultureInvariant;
+            if (Check_ECMAScript.IsChecked == true) options = options | RegexOptions.ECMAScript;
+            if (Check_ExplicitCapture.IsChecked == true) options = options | RegexOptions.ExplicitCapture;
+            if (Check_IgnoreCase.IsChecked == true) options = options | RegexOptions.IgnoreCase;
+            if (Check_IgnorePatternWhitespace.IsChecked == true) options = options | RegexOptions.IgnorePatternWhitespace;
+            if (Check_Multiline.IsChecked == true) options = options | RegexOptions.Multiline;
+            if (Check_RightToLeft.IsChecked == true) options = options | RegexOptions.RightToLeft;
+            if (Check_Singleline.IsChecked == true) options = options | RegexOptions.Singleline;
+
+            Regex regex;
+            Match match;
+            MatchCollection matchCollection;
+            
+            regex = new Regex(@""+model.Regex, options);
+            matchCollection = regex.Matches(model.FileContent);
+
+            model.Results.Clear();
+            model.Results.Add("Result Count : "+ matchCollection.Count+"\n\n");
+            if (matchCollection.Count > 0)
+            {
+                for (int i = 0; i < matchCollection.Count; i++)
+                {
+                    match = matchCollection[i];
+                    {
+                        model.Results.Add(match.Value);
+                    }
+                }
+            }
+            LB.Items.Refresh();
+        }
+    }
+}
